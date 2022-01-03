ab0a4cf0addf263697e72d7116272b9e424aba0d
Hyrum14 <59519173+Hyrum14@users.noreply.github.com>
2022-01-03 14:11:46 -0700

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -1,14 +1,24 @@
 import random
-import subprocess
-import collections
 import math
 
 class Network:
     """
-    Neural network
+    Creates a neural network. 
+
+    For each layer put an argument with the following format:
+    [[value, bias, [weight0, weight1, ... weightN]], [...], ... [...]]
+
+    Value: the number stored in the vertex
+    Bias: Added to product when determining value
+    Weights: Effect on each of the vertexes in the next layer
+
+    You can create a blank (all zeros) network by using keyword 'layers' with the following format:
+    [layer0Size, layer1Size, ..., layerNSize]
     """
-    def __init__(self, layers=[]):
-        self.points = [[[0, 0, [0 for n in range(layers[(icount + 1) % len(layers)])]] for j in range(i)] for (icount, i) in enumerate(layers)]
+    def __init__(self, *args, **kwargs):
+        self.points = args
+        if 'layers' in kwargs:
+            self.points = [[[0, 0, [0 for n in range(kwargs['layers'][(icount+1) % len(kwargs['layers'])])]] for j in range(i)] for (icount, i) in enumerate(kwargs['layers'])]
 
     def show(self, full=False):
         for i in self.points:
@@ -21,12 +31,12 @@ class Network:
                     print('')
             print('')
 
-    def scramble(self):
+    def scramble(self, *, prevailance=0):
         for i in self.points:
             for j in i:
-                j[0] = round(random.uniform(0, 1), 2)
-                j[1] = round(random.uniform(0, 1), 2)
-                j[2] = [round(random.uniform(0, 1), 2) for _ in range(len(j[2]))]
+                j[0] = round(random.uniform(0, 1) + j[0] * prevailance, 2)
+                j[1] = round(random.uniform(0, 1) + j[1] * prevailance, 2)
+                j[2] = [round(random.uniform(0, 1) + i * prevailance, 2) for i in j[2]]
         return self.points
     
     def calcButBetter(self):
@@ -52,14 +62,14 @@ class Network:
         return self.points
     
     def save(self, name):
-        saved = open('./NeuralNetwork/saved.txt', 'a')
+        saved = open('saved.txt', 'a')
         saved.write(name.lower())
         saved.write('|')
         saved.write(str(self.points))
         saved.write('\n')
     
     def load(self):
-        saved = open('./NeuralNetwork/saved.txt', 'r')
+        saved = open('saved.txt', 'r')
         networks = saved.read().split('\n')
         names = list(map(lambda a: a[:a.index('|')], networks[:-1]))
         print(names)
@@ -69,7 +79,7 @@ class Network:
 
 
 if __name__ == '__main__':
-    a = Network([5, 2, 2, 4])
-    a.save('joe')
+    a = Network(layers=[5, 2, 2, 4])
+    a.save(input('Saved Name: '))
     a.load()
     a.show()
